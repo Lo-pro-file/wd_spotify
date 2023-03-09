b1f3106480c3653cc1a34f37f383ac92c68cf04b
WD Zone <117820644+angel-loveyou@users.noreply.github.com>
2023-03-09 23:47:15 +0530

    op

--- mbot/plugins/deezer.py
+++ mbot/plugins/deezer.py
@@ -45,7 +45,7 @@ async def link_handler(_, message):
             randomdir = f"/tmp/{str(randint(1,100000000))}"
             mkdir(randomdir)
             for song in songs:
-                PForCopy = await message.reply_photo(song.get('cover'),caption=f"🎧 Title : `{song['name']}`\n🎤 Artist : `{song['artist']}`\n💽 Album : `{song['album']}`\n💽 Song Number : `{song['playlist_num']}`")
+                PForCopy = await message.reply_photo(song.get('cover'),caption=f"✤ 𝐓𝐢𝐭𝐥𝐞 ● `{song['name']}`\n✤ 𝐀𝐫𝐭𝐢𝐬𝐭 ● `{song['artist']}`\n✤ 𝐀𝐥𝐛𝐮𝐦 ● `{song['album']}`\n✤ 𝐒𝐨𝐧𝐠 𝐍𝐮𝐦𝐛𝐞𝐫 ● `{song['playlist_num']}`")
                 path = await download_songs(song,randomdir)
                 thumbnail = await thumb_down(song.get('thumb'),song.get('name'))
                 AForCopy = await message.reply_audio(path,performer=song.get('artist'),title=f"{song.get('name')} - {song.get('artist')}",caption=f"[{song['name']}](https://www.deezer.com/track/{song['deezer_id']}) | {song['album']} - {song['artist']}",thumb=thumbnail,duration=song['duration'])
